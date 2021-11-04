93c5262dc4874663f84c414f79d6a3f9426e985c
kenkaycee <petezenta@gmail.com>
2021-11-04 13:43:57 +0000

    Create Task 2.py

---
+++ Home Learning Tasks/Week 3 HL/Numpy HL/Task 2.py
@@ -0,0 +1,10 @@
+"""
+Create a 3×3 NumPy array of all Boolean value Trues
+"""
+
+# Solution 
+
+import numpy as np
+
+true_array= np.full(shape= (3,3), fill_value= True, dtype =bool)
+print(true_array)
